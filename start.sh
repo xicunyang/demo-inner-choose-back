571050effd845675ceebe2b072b274caf444dbef
杨喜存 <xicunyang@163.com>
2020-05-28 18:00:16 +0800

    bugfix: 增加start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+# 拉取代码
+git pull
+
+# 安装依赖和打包
+yarn --registry=https://registry.npm.taobao.org/ && yarn build
+
+# 删除容器
+docker rm -f vue-template &> /dev/null
+
+#启动容器
+docker run -d --restart=on-failure:5\
+    -p 9999:80 \
+    -v $PWD/dist:/usr/share/nginx/html \
+    --name vue-template nginx
+
+
